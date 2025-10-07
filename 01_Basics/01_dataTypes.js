d7d4098ffe8d1e22c0907e74e43824d6a7e8586d
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-10-07 13:03:11 +0530

    datatypes.js

--- 01_Basics/01_dataTypes.js
+++ 01_Basics/01_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Vicky");
 
 
-let name = "hitesh"
+let name = "vicky" // string
 let age = 18
 let isLoggedIn = false
 let state;
